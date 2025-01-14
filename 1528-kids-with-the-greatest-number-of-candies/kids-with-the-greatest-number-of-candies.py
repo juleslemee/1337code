99ffa2cee3e17d74175e52ebaad3d898e01ec467
Jules <106885248+juleslemee@users.noreply.github.com>
2025-01-13 23:04:31 -0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (7.09%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -1,20 +1,12 @@
 class Solution(object):
     def kidsWithCandies(self, candies, extraCandies):
-        """
-        :type candies: List[int]
-        index is the child number. value is their number of candies
-        :type extraCandies: int
-        all the extras i have to give to the ith person
-        :rtype: List[bool]
-        """
         # result [] will be the 'victorystatus' of the ith children
         result = []
+        # find the max in here
+        maxlist = (sorted(candies, reverse=True))
+        # take max - extraCandies
+        toBeat = (maxlist[0] - extraCandies)
         for i in range(len(candies)):
-            print(candies[i])
-            # find the max in here
-            maxlist = (sorted(candies, reverse=True))
-            # take max - extraCandies
-            toBeat = (maxlist[0] - extraCandies)
             # all elements with this value or greater will have 'true'
             if candies[i] >= toBeat:
                 result.append(True)
