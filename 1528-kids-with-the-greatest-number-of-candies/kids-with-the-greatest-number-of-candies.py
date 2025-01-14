60280a6b2376252fbad605eb68db7df26d959fd9
Jules <106885248+juleslemee@users.noreply.github.com>
2025-01-13 23:07:16 -0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (7.09%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -2,9 +2,10 @@ class Solution(object):
     def kidsWithCandies(self, candies, extraCandies):
         # result [] will be the 'victorystatus' of the ith children
         result = []
+        toBeat = (max(candies) - extraCandies)
         for i in range(len(candies)):
             # all elements with this value or greater will have 'true'
-            if candies[i] >= (max(candies) - extraCandies):
+            if candies[i] >= toBeat:
                 result.append(True)
             else:
                 result.append(False)
