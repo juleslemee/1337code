7a72e8702d677260b49b0c22b26b74b790857fff
Jules <106885248+juleslemee@users.noreply.github.com>
2025-01-16 11:28:32 -0500

    Time: 7 ms (91.96%) | Memory: 18.4 MB (56.58%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -1,17 +1,21 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
         vowels = {'a','A','e','E','i','I','o','O','u','U'}
+        # list will make it easier to traverse
         chars = list(s)
-        foundVowels = []
-        for letter in s:
-            if letter in vowels:
-                foundVowels.append(letter)
-        foundVowels = list(reversed(foundVowels))
-        i = 0
-        j = 0
-        for letter in s:
-            if letter in vowels:
-                chars[i] = foundVowels[j]
-                j += 1
-            i += 1
+        # left is the beginning, right is the end (index starts at 0, so len-1)
+        left, right = 0, len(chars)-1
+        # until we've finished iterating from both sides and the pointers cross
+        while left < right:
+            # if left not in vowels, we can move it forward
+            while left < right and chars[left] not in vowels:
+                left += 1
+            # if right not in vowels, we can move it back 
+            while left < right and chars[right] not in vowels:
+                right -= 1
+            # once they are both in vowels, we'll swap them and increment both (+ & -)
+            chars[left], chars[right] = chars[right], chars[left]
+            left += 1
+            right -= 1
+        # we need to get the list back into a string, no separator needed
         return ''.join(chars)
