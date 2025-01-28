3698cc3eeda195b9f47e8fe59b33a23a0979ebcb
Jules <106885248+juleslemee@users.noreply.github.com>
2025-01-28 16:38:26 -0500

    Time: 71 ms (42.17%) | Memory: 21.8 MB (52.59%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -4,10 +4,12 @@ class Solution:
         maxSize = 0
 
         for r, num in enumerate(nums):
-            count += (1 if num==0 else 0)
+            if num==0:
+                count += 1
             
             while count > 1:
-                count -= (1 if nums[l]==0 else 0)
+                if nums[l]==0:
+                    count -= 1
                 l += 1
 
             maxSize = max(maxSize, r - l)
