2ff761ce8305fd5d520dd24397eefa345c8d979d
Jules <106885248+juleslemee@users.noreply.github.com>
2025-01-28 16:03:15 -0500

    Time: 2649 ms (65.37%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,18 +1,10 @@
 /* Write your T-SQL query statement below */
-SELECT P.project_id, AVG(AY.sumE) as average_years
+SELECT P.project_id, ROUND(SUM(E.experience_years) * 1.0 / COUNT(E.experience_years), 2) as average_years
 FROM
 Project as P
-LEFT JOIN
-(
-    SELECT project_id, ROUND(SUM(E.experience_years) * 1.0 / COUNT(E.experience_years), 2) as sumE
-    FROM
-    Project as P
-    LEFT JOIN
-    Employee as E
-    ON
-    P.employee_id = E.employee_id
-    GROUP BY project_id
-) as AY
-ON P.project_id = AY.project_id
-GROUP BY P.project_id
+JOIN
+Employee as E
+ON
+P.employee_id = E.employee_id
+GROUP BY project_id
 ORDER BY P.project_id
