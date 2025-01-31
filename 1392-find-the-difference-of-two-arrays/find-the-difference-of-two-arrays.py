ee8d5fcf40e0ecd28dda7e39930f84a160627f5b
Jules <106885248+juleslemee@users.noreply.github.com>
2025-01-31 15:46:33 -0500

    Time: 4 ms (85.26%) | Memory: 18 MB (52.68%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
@@ -2,6 +2,5 @@ class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
         list1 = list(set(nums1)-set(nums2))
         list2 = list(set(nums2)-set(nums1)) 
-        answer = [list1, list2]
-        return answer
+        return [list1, list2]
         
