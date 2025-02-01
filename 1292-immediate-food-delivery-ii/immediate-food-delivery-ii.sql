baa231f26a8bcb2d850edc0f9a6e9a9e5a34d745
Jules <106885248+juleslemee@users.noreply.github.com>
2025-02-01 13:37:24 -0500

    Time: 2334 ms (48.60%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -4,21 +4,16 @@
 -- I want to count how many order_date = customer_pref_delivery_date (immediate orders) 
 -- / order_date (all orders, immediate+scheduled)
 -- all of these have to be the first order of a customer, i.e. the earliest order_date
-WITH first_immediate AS (
-    SELECT d.customer_id, MIN(d.order_date) as mins
-    FROM Delivery as d
-    WHERE d.order_date = (
-        SELECT MIN(d2.order_date) 
-        FROM Delivery d2
-        WHERE d2.customer_id = d.customer_id
-    )
-    AND d.order_date = d.customer_pref_delivery_date
-    GROUP BY d.customer_id
-), 
-all_first AS (
-    SELECT d3.customer_id, MIN(order_date) as mins3
-    FROM Delivery as d3
-    GROUP BY d3.customer_id
+WITH all_first AS (
+    SELECT customer_id, MIN(order_date) as min_order_date
+    FROM Delivery
+    GROUP BY customer_id
+),
+first_immediate AS (
+    SELECT af.customer_id, af.min_order_date
+    FROM all_first AS af
+    JOIN Delivery d ON d.customer_id = af.customer_id AND d.order_date = af.min_order_date
+    WHERE d.order_date = d.customer_pref_delivery_date
 )
 
 SELECT 
