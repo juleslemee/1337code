7610fc14b44418f2aa50ed76f85ee4a47a6cb34e
Jules <106885248+juleslemee@users.noreply.github.com>
2025-02-01 13:44:33 -0500

    Time: 1862 ms (72.21%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -7,6 +7,6 @@ JOIN (
     SELECT customer_id, MIN(order_date) AS min_order_date
     FROM Delivery
     GROUP BY customer_id
-) first_orders 
+) AS first_orders 
 ON d.customer_id = first_orders.customer_id 
 AND d.order_date = first_orders.min_order_date;
