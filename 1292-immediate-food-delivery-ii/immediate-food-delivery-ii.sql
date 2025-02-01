2715508ed082e8876a2fc5fdb26601d8d309225e
Jules <106885248+juleslemee@users.noreply.github.com>
2025-02-01 13:38:07 -0500

    Time: 2621 ms (31.70%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,9 +1,4 @@
 /* Write your T-SQL query statement below */
-
-
--- I want to count how many order_date = customer_pref_delivery_date (immediate orders) 
--- / order_date (all orders, immediate+scheduled)
--- all of these have to be the first order of a customer, i.e. the earliest order_date
 WITH all_first AS (
     SELECT customer_id, MIN(order_date) as min_order_date
     FROM Delivery
@@ -15,7 +10,6 @@ first_immediate AS (
     JOIN Delivery d ON d.customer_id = af.customer_id AND d.order_date = af.min_order_date
     WHERE d.order_date = d.customer_pref_delivery_date
 )
-
 SELECT 
 ROUND(COUNT(fi.customer_id)*100.0/ COUNT(af.customer_id), 2) as immediate_percentage
 FROM
