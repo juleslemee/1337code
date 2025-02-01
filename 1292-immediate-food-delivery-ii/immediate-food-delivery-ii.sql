1a22737c6dba97400d2b3bdf787f614201c19faa
Jules <106885248+juleslemee@users.noreply.github.com>
2025-02-01 13:44:47 -0500

    Time: 2204 ms (55.92%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -2,7 +2,7 @@
 SELECT 
     ROUND(SUM(CASE WHEN d.order_date = d.customer_pref_delivery_date THEN 1 ELSE 0 END) 
         * 100.0 / COUNT(DISTINCT d.customer_id), 2) AS immediate_percentage
-FROM Delivery d
+FROM Delivery AS d
 JOIN (
     SELECT customer_id, MIN(order_date) AS min_order_date
     FROM Delivery
