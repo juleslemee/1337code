34434a1eca82ba0b6a7cbe6d17a17bc06d3bfecd
Jules <106885248+juleslemee@users.noreply.github.com>
2025-02-02 12:04:05 -0500

    Time: 155 ms (34.40%) | Memory: 18.1 MB (77.29%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -6,6 +6,4 @@ class Solution:
             hash1[char1] = hash1.get(char1, 0) + 1
         for char2 in word2:
             hash2[char2] = hash2.get(char2, 0) + 1
-        if sorted(list(hash1.keys())) == sorted(list(hash2.keys())) and sorted(list(hash1.values())) == sorted(list(hash2.values())):
-            return True
-        return False
+        return sorted(list(hash1.keys())) == sorted(list(hash2.keys())) and sorted(list(hash1.values())) == sorted(list(hash2.values()))
