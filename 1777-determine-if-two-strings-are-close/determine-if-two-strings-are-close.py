d12d191711250418e0a2f6220b31c94c572fef56
Jules <106885248+juleslemee@users.noreply.github.com>
2025-02-02 12:20:50 -0500

    Time: 198 ms (9.65%) | Memory: 18.3 MB (17.33%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -1,17 +1,15 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        hash1 = {}
-        hash2 = {}
-        for char in word1:
-            hash1[char] = hash1.get(char, 0) + 1
-        for char in word2:
-            hash2[char] = hash2.get(char, 0) + 1
+        if len(word1) != len(word2):  # Quick exit
+            return False
 
-        counth1 = {}
-        counth2 = {}
-        for count in hash1.values():
-            counth1[count] = counth1.get(count, 0) + 1
-        for count in hash2.values():
-            counth2[count] = counth2.get(count, 0) + 1
+        hash1, hash2 = {}, {}
 
-        return set(hash1.keys()) == set(hash2.keys()) and counth1 == counth2
+        for i in range(len(word1)):
+            hash1[word1[i]] = hash1.get(word1[i], 0) + 1
+            hash2[word2[i]] = hash2.get(word2[i], 0) + 1
+
+        if hash1.keys() != hash2.keys():
+            return False
+
+        return sorted(hash1.values()) == sorted(hash2.values())
