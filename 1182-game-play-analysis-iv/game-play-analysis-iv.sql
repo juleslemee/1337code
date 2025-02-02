a4bd594ea3978caee63ab9ab2b4540c7d74fb07d
Jules <106885248+juleslemee@users.noreply.github.com>
2025-02-02 12:37:29 -0500

    Time: 3774 ms (34.83%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -6,7 +6,7 @@ WITH fd AS (
 ), lb AS (
     SELECT fd.player_id as player_id
     FROM Activity as A
-    LEFT JOIN fd
+    JOIN fd
     ON A.player_id = fd.player_id
     WHERE A.event_date = DATEADD(day, 1, fd.first_day)
 )
@@ -15,4 +15,3 @@ SELECT ROUND(COUNT(lb.player_id) * 1.0 / COUNT(fd.player_id), 2) as fraction
 FROM fd
 LEFT JOIN lb
 ON fd.player_id = lb.player_id
-
