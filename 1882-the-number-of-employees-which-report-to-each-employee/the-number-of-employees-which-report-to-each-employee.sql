afeb4e877a996705dd105eacbc6b82f31b234b6a
Jules <106885248+juleslemee@users.noreply.github.com>
2025-02-08 20:33:19 -0500

    Time: 1690 ms (53.87%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,5 +1,4 @@
 /* Write your T-SQL query statement below */
-
 SELECT
 E.employee_id, E.name, R.reports_count, CASE 
         WHEN R.avg_age - FLOOR(R.avg_age) >= 0.5 THEN FLOOR(R.avg_age) + 1
@@ -15,26 +14,3 @@ INNER JOIN
 ) as R
 ON
 E.employee_id = R.reports_to
-
-/*
-SELECT 
-    D.employee_id,
-    D.name,
-    D.reports_count,
-    CASE 
-        WHEN D.avg_age - FLOOR(D.avg_age) >= 0.5 THEN FLOOR(D.avg_age) + 1
-        ELSE FLOOR(D.avg_age)
-    END AS average_age
-FROM (
-    SELECT 
-        m.employee_id,
-        m.name,
-        COUNT(*) AS reports_count,
-        AVG(CAST(r.age AS DECIMAL(10,2))) AS avg_age
-    FROM Employees AS m
-    JOIN Employees AS r 
-      ON r.reports_to = m.employee_id
-    GROUP BY m.employee_id, m.name
-) AS D
-ORDER BY D.employee_id;
-*/
