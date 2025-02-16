47f523aabbe71c6c525ce000accbe41ece485374
Jules <106885248+juleslemee@users.noreply.github.com>
2025-02-16 18:06:18 -0500

    Time: 158 ms (19.81%) | Memory: 32.3 MB (33.66%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -4,23 +4,12 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-# basically, a node is good if nothing that comes before it on the way to traversal is greater than itself. 
-# So I guess I would need to do some kind of DFS traversal, Root->left->right
-# I would want to store the node and all the nodes that lead up to it. 
-
 class Solution:
-    def goodNodes(self, root: TreeNode) -> int:
-        return self.countGoods(root)
-    def countGoods(self, root, maxSoFar=float('-inf')):
+    def goodNodes(self, root: TreeNode, maxSoFar=float('-inf')) -> int:
         if root is None:
             return 0
-        
         count = 1 if root.val >= maxSoFar else 0
-
         newMax = max(maxSoFar, root.val)
-
-        count += self.countGoods(root.left, newMax)
-        count += self.countGoods(root.right, newMax)
-        
+        count += self.goodNodes(root.left, newMax)
+        count += self.goodNodes(root.right, newMax)
         return count
